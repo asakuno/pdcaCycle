8358d1b305273b1e2cfc90c2f39286ec6200b68f
asakuno <takahiro.asakuno@gmail.com>
2023-12-13 03:30:13 +0900

    [Add]ボタンにcss当てる

--- src/app/components/button/move_button.tsx
+++ src/app/components/button/move_button.tsx
@@ -3,7 +3,16 @@ import React from "react";
 interface MoveButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   title: string;
+  buttonStyle: string;
 }
-export const MoveButton = ({ onClick, title }: MoveButtonProps) => {
-  return <button onClick={onClick}>{title}</button>;
+export const MoveButton = ({
+  onClick,
+  title,
+  buttonStyle,
+}: MoveButtonProps) => {
+  return (
+    <button onClick={onClick} className={buttonStyle}>
+      {title}
+    </button>
+  );
 };
