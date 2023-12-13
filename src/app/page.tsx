3d79f084a487abbe85b1190ba39020b5c633f799
asakuno <takahiro.asakuno@gmail.com>
2023-12-14 02:30:18 +0900

    余計なインポートの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, MouseEvent } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import pdca from "../../public/images/pdca.png";
 import "@/app/ui/rotate.scss";
